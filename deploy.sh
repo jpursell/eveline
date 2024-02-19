90a892d5c0043c5ea0b0903d366f3fe8822c5723
Jeff Pursell <jpursell@gmail.com>
2024-02-19 14:36:20 -0500

    Add config and deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly TARGET_HOST=jpursell@iprj
+readonly TARGET_PATH=/home/jpursell/eveline
+readonly TARGET_ARCH=arm-unknown-linux-gnueabihf
+readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/eveline
+
+cargo build --release --target=${TARGET_ARCH}
+rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+ssh -t ${TARGET_HOST} ${TARGET_PATH}
