c53a9cfec002ae520f6ed0c1c755cbf35cc6c0d4
Jeff Pursell <jpursell@gmail.com>
2024-02-19 17:39:51 -0500

    Go up and down sorta

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,92 @@
 use std::error::Error;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::thread;
-use std::time::Duration;
+use std::time::Instant;
 
 // The simple-signal crate is used to handle incoming signals.
 use simple_signal::{self, Signal};
 
-use rppal::gpio::Gpio;
+use rppal::gpio::{Gpio, OutputPin};
 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
 const RIGHT_PINS: [u8; 4] = [2, 17, 3, 4];
 const LEFT_PINS: [u8; 4] = [12, 21, 16, 20];
 
+struct Motor {
+    pins: [OutputPin; 4],
+    current: usize,
+    position: i32,
+    side: Side,
+}
+
+enum Side {
+    Left,
+    Right,
+}
+impl Motor {
+    fn new(side: Side) -> Motor {
+        // init output pins
+        let pin_nums = match side {
+            Side::Left => LEFT_PINS,
+            Side::Right => RIGHT_PINS,
+        };
+        let gpio = Gpio::new().unwrap();
+        let make_output = |n| gpio.get(n).unwrap().into_output();
+        let mut pins = [
+            make_output(pin_nums[0]),
+            make_output(pin_nums[1]),
+            make_output(pin_nums[2]),
+            make_output(pin_nums[3]),
+        ];
+        // set initial pin value
+        let current = 0;
+        pins[current].set_high();
+        pins.iter_mut().skip(1).for_each(|pin| pin.set_low());
+        Motor {
+            pins,
+            current,
+            position: 0,
+            side,
+        }
+    }
+    fn step_down(&mut self) {
+        match self.side {
+            Side::Left => self.step_clock_wise(),
+            Side::Right => self.step_counter_clock_wise(),
+        };
+        self.position -= 1;
+    }
+    fn step_up(&mut self) {
+        match self.side {
+            Side::Right => self.step_clock_wise(),
+            Side::Left => self.step_counter_clock_wise(),
+        };
+        self.position += 1;
+    }
+    fn step_clock_wise(&mut self) {
+        let next = (self.current + 1) % self.pins.len();
+        self.pins[next].set_high();
+        self.pins[self.current].set_low();
+        self.current = next;
+    }
+    fn step_counter_clock_wise(&mut self) {
+        let next = {
+            if self.current == 0 {
+                self.pins.len() - 1
+            } else {
+                self.current - 1
+            }
+        };
+        self.pins[next].set_high();
+        self.pins[self.current].set_low();
+        self.current = next;
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Retrieve the GPIO pins and configure them as outputs.
-    let mut right_pins = RIGHT_PINS
-        .iter()
-        .map(|&x| Gpio::new().unwrap().get(x).unwrap().into_output())
-        .collect::<Vec<_>>();
-    let mut left_pins = LEFT_PINS
-        .iter()
-        .map(|&x| Gpio::new().unwrap().get(x).unwrap().into_output())
-        .collect::<Vec<_>>();
-    right_pins.iter_mut().skip(1).for_each(|x| {
-        x.set_low();
-    });
-    right_pins[0].set_high();
-    left_pins.iter_mut().skip(1).for_each(|x| {
-        x.set_low();
-    });
-    left_pins[0].set_high();
+    let mut right_motor = Motor::new(Side::Right);
+    let mut left_motor = Motor::new(Side::Left);
 
     let delay = {
         let rpm = 100;
@@ -51,15 +107,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Blink the LED until running is set to false.
-    let mut current = 0;
+    let mut last_time = Instant::now();
+    let mut target_position = 100;
     while running.load(Ordering::SeqCst) {
-        let next = (current + 1) % 4;
-        right_pins[next].set_high();
-        left_pins[next].set_high();
-        right_pins[current].set_low();
-        left_pins[current].set_low();
-        current = next;
-        thread::sleep(Duration::from_millis(delay));
+        if last_time.elapsed().as_millis() >= delay {
+            last_time = Instant::now();
+            if left_motor.position == target_position {
+                target_position += 100;
+                target_position %= 200;
+            }
+            if left_motor.position > target_position {
+                left_motor.step_down();
+            } else {
+                left_motor.step_up();
+            }
+            if right_motor.position > target_position {
+                right_motor.step_down();
+            } else {
+                right_motor.step_up();
+            }
+        }
     }
 
     Ok(())
