fdf75286aa58a0ee1ea651722eb9275258c3e0a6
Jeff Pursell <jpursell@gmail.com>
2024-05-04 08:27:03 -0400

    Work on gclode

--- src/gcode.rs
+++ src/gcode.rs
@@ -2,10 +2,47 @@ use anyhow::Result;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
-    path::Path,
+    path::Path, str::FromStr,
 };
 
-struct GCode {}
+enum GCommand {
+    FastMove,
+    Move,
+}
+
+struct GCode {
+    command: Option<GCommand>,
+    comment: Option<String>,
+    x: Option<f64>,
+    y: Option<f64>,
+    z: Option<f64>,
+    f: Option<f64>,
+}
+
+impl FromStr for GCode {
+    type Err = ();
+
+    /// Parse gcode strings i.e. 
+    /// (comment)
+    /// G21 (comment)
+    /// G1 F8000 (set speed)
+    /// G0 X15.254 Y82.542
+    /// G1 Z0
+    fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
+        let mut s = s.trim();
+        let l_paren = s.find('(');
+        if l_paren.is_some() {
+            let r_paren = s.find(')');
+            if r_paren.is_none() {
+                return Err(())
+            }
+
+
+
+        }
+        todo!()
+    }
+}
 
 pub struct GCodeFile {
     codes: Vec<GCode>,
